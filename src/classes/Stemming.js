3565a68253524e6226126a31bd38ec553f3d689b
Nepein Andrey <nepein96@gmail.com>
2018-11-11 20:36:42 +0300

    Stemming less code

--- src/classes/Stemming.js
+++ src/classes/Stemming.js
@@ -46,30 +46,30 @@ class StemmingClass {
         let i = 0;
         while (i < word.length - 1) {
             if (word[i].in(this.consonantCodes) && word[i + 1].in(this.vowelCodes))
-                return i += 2;
+                return [word.slice(0, i += 2), word.slice(i)];
             i++;
         }
-        return false;
+        return [word, ''];
     }
 
     getR1(word) {
         let i = 0;
         while (i < word.length - 1) {
             if (word[i].in(this.vowelCodes) && word[i + 1].in(this.consonantCodes))
-                return i += 2;
+                return [word.slice(0, i += 2), word.slice(i)];
             i++;
         }
-        return false;
+        return [word, ''];
     }
 
-    getR2(rv) {
+    getR2(r1) {
         let i = 0;
-        while (i < rv.length - 1) {
-            if (rv[i].in(this.vowelCodes) && rv[i + 1].in(this.consonantCodes))
-                return i += 2;
+        while (i < r1.length - 1) {
+            if (r1[i].in(this.vowelCodes) && r1[i + 1].in(this.consonantCodes))
+                return [r1.slice(0, i += 2), r1.slice(i)];
             i++;
         }
-        return false;
+        return [r1, ''];
     }
 
     stemWords(words) {
@@ -77,7 +77,6 @@ class StemmingClass {
         let stemmedWord = '';
         for (let i = 0; i < words.length; i++) {
             stemmedWord = this.stemWord(words[i]);
-            // if (stemmedWord !== '') 
             stemmedWords.push(stemmedWord);
         }
         return stemmedWords;
@@ -95,8 +94,12 @@ class StemmingClass {
             if (word.length >= lengths[i]) {
                 pos = word.length - lengths[i];
                 ending = word.slice(pos);
-                if (endings.length === 3) find = (word[pos - 1] === 'я' || word[pos - 1] === 'а') ? endings[0] : endings[1];
-                else find = endings[0];
+
+                if (endings.length === 3)
+                    find = (word[pos - 1] === 'я' || word[pos - 1] === 'а') ? endings[0] : endings[1];
+                else
+                    find = endings[0];
+
                 if (ending.inString(find)) {
                     word = word.slice(0, word.length - ending.length);
                     if (endings === this.adjective) {
@@ -111,167 +114,29 @@ class StemmingClass {
         return false;
     }
 
-    perfectiveGerundF(word) {
-        let ending = '';
-        let find = [];
-        let pos = 0;
-
-        for (let i = 0; i < this.perfectiveGerund[2].length; i++) {
-            pos = word.length - this.perfectiveGerund[2][i];
-            if (word.length >= this.perfectiveGerund[2][i]) {
-                ending = word.slice(pos);
-                find = (word[pos - 1] === 'я' || word[pos - 1] === 'а') ? this.perfectiveGerund[0] : this.perfectiveGerund[1];
-                if (ending.inString(find))
-                    return word.slice(0, word.length - ending.length)
-            }
-        }
-        return false;
-    }
-
-    reflexiveF(rv) {
-        let ending = '';
-        let pos = 0;
-        for (let i = 0; i < this.reflexive[1].length; i++) {
-            pos = rv.length - this.reflexive[1][i];
-            if (rv.length >= this.reflexive[1][i]) {
-                ending = rv.slice(pos);
-                if (ending.inString(this.reflexive[0])) {
-                    rv = rv.slice(0, rv.length - ending.length);
-                    return rv;
-                    break;
-                }
-            }
-        }
-        return false;
-    }
-
-    adjectiveF(rv) {
-        let ending = '';
-        let pos = 0;
-        for (let i = 0; i < this.adjective[1].length; i++) {
-            pos = rv.length - this.adjective[1][i];
-            if (rv.length >= this.adjective[1][i]) {
-                ending = rv.slice(pos);
-                if (ending.inString(this.adjective[0])) {
-                    rv = rv.slice(0, rv.length - ending.length);
-                    let p = this.participleF(rv);
-                    if (p)
-                        rv = p;
-                    return rv;
-                    break;
-                }
-            }
-        }
-        return false;
-    }
-
-    participleF(word) {
-        let ending = '';
-        let find = [];
-        let pos = 0;
-
-        for (let i = 0; i < this.participle[2].length; i++) {
-            pos = word.length - this.participle[2][i];
-            if (word.length >= this.participle[2][i]) {
-                ending = word.slice(pos);
-                find = (word[pos - 1] === 'я' || word[pos - 1] === 'а') ? this.participle[0] : this.participle[1];
-                if (ending.inString(find))
-                    return word.slice(0, word.length - ending.length)
-            }
-        }
-        return false;
-    }
-
-    verbF(word) {
-        let ending = '';
-        let find = [];
-        let pos = 0;
-
-        for (let i = 0; i < this.verb[2].length; i++) {
-            pos = word.length - this.verb[2][i];
-            if (word.length >= this.verb[2][i]) {
-                ending = word.slice(pos);
-                find = (word[pos - 1] === 'я' || word[pos - 1] === 'а') ? this.verb[0] : this.verb[1];
-                if (ending.inString(find))
-                    return word.slice(0, word.length - ending.length)
-            }
-        }
-        return false;
-    }
-
-    nounF(rv) {
-        let ending = '';
-        let pos = 0;
-        console.log('noun', rv)
-        for (let i = 0; i < this.noun[1].length; i++) {
-            if (rv.length >= this.noun[1][i]) {
-                pos = rv.length - this.noun[1][i];
-                ending = rv.slice(pos);
-                if (ending.inString(this.noun[0])) {
-                    console.log('found', rv.slice(0, rv.length - ending.length))
-                    rv = rv.slice(0, rv.length - ending.length);
-                    return rv;
-                    break;
-                }
-            }
-        }
-        return false;
-    }
-
-    superlativeF(rv) {
-        let ending = '';
-        let pos = 0;
-        for (let i = 0; i < this.superlative[1].length; i++) {
-            pos = rv.length - this.superlative[1][i];
-            if (rv.length >= this.superlative[1][i]) {
-                ending = rv.slice(pos);
-                if (ending.inString(this.superlative[0])) {
-                    rv = rv.slice(0, rv.length - ending.length);
-                    return rv;
-                    break;
-                }
-            }
-        }
-        return false;
-    }
-
-    derivationalF(rv) {
-        let ending = '';
-        let pos = 0;
-        for (let i = 0; i < this.derivational[1].length; i++) {
-            pos = rv.length - this.derivational[1][i];
-            if (rv.length >= this.derivational[1][i]) {
-                ending = rv.slice(pos);
-                if (ending.inString(this.derivational[0])) {
-                    rv = rv.slice(0, rv.length - ending.length);
-                    return rv;
-                    break;
-                }
-            }
-        }
-        return false;
-    }
-
     firstStep(word) {
-        let rv = word.slice(this.getRV(word));
         let reflexiveWasFound = false;
+        let pg, r, a, v, n;
+        let rv = this.getRV(word)[1];;
 
-        let pg = this.findEnding(this.perfectiveGerund, word);
+        pg = this.findEnding(this.perfectiveGerund, word);
         if (pg !== false) return pg;
 
-        let r = this.findEnding(this.reflexive, rv);
+        if (rv.length <= 2)
+            return rv;
+
+        r = this.findEnding(this.reflexive, rv);
         if (r !== false) {
             reflexiveWasFound = true;
             rv = r;
         }
 
-        let a = this.findEnding(this.adjective, rv);
+        a = this.findEnding(this.adjective, rv);
         if (a != false) return a;
 
-        let v = this.findEnding(this.verb, rv);
-        if (v) return rv = v;
+        v = this.findEnding(this.verb, rv);
+        if (v !== false) return rv = v;
 
-        let n;
         if (!reflexiveWasFound) {
             n = this.findEnding(this.noun, rv);
             if (n !== false)
@@ -290,45 +155,48 @@ class StemmingClass {
     thirdStep(word) {
         if (word.length >= 3) {
             let d = this.findEnding(this.derivational, word);
-            console.log(d);
-            if (d != false)
-                return d;
-            else
-                return word;
+            return (d != false) ? d : word;
         }
         return word;
     }
 
     fourthStep(word) {
-        let s = '';
+        let s;
+
         if (word[word.length - 1] === 'н' && word[word.length - 2] === 'н')
             return word.slice(0, word.length - 1);
-        // s = this.superlativeF(word);
-        s = this.findEnding(this.superlative, word);
 
-        if (s)
-            return s;
+        s = this.findEnding(this.superlative, word);
+        if (s !== false) return s;
+
         if (word[word.length - 1] === 'ь')
             return word.slice(0, word.length - 1);
+
         return word;
     }
 
     stemWord(word) {
-        let head = word.slice(0, this.getRV(word));
+        // стэмминг для слов с дефисом 
 
-        let first = this.firstStep(word);
-        let second = this.secondStep(first);
+        let rvhead = this.getRV(word)[0];
+        let first, second, third, fourth;
+        let cuttedWord;
+        let r1head, r1, r2head, r2;
 
-        let newword = head + second;
-        let r1head = newword.slice(0, this.getR1(newword));
-        let r1 = newword.slice(this.getR1(newword));
+        first = this.firstStep(word); // первый шаг
+        second = this.secondStep(first); // второй шаг
 
-        let r2head = r1.slice(0, this.getR2(r1));
-        let r2 = r1.slice(this.getR2(r1));
+        cuttedWord = rvhead + second;
+        r1head = this.getR1(cuttedWord)[0];
+        r1 = this.getR1(cuttedWord)[1];
+        r2head = this.getR2(r1)[0];
+        r2 = this.getR2(r1)[1];
 
-        let third = this.thirdStep(r2);
-        newword = r1head + r2head + third;
-        let fourth = this.fourthStep(newword);
+        third = this.thirdStep(r2); // третий шаг
+
+        cuttedWord = r1head + r2head + third;
+        fourth = this.fourthStep(cuttedWord); // четвёртый шаг
+
         return fourth;
     }
 }
