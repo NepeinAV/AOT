53c7f79cfb6e5e3bac30857af13ffbef28ea7833
Nepein Andrey <nepein96@gmail.com>
2018-11-19 21:21:51 +0300

    R fixes

--- src/classes/SearchEngine.js
+++ src/classes/SearchEngine.js
@@ -12,10 +12,30 @@ class SearchEngine {
         return poz;
     }
 
+    /*
+        Вес слов ПОЗ, отсутствущих в списке дескрипторов, вычисляется по формуле 
+
+                                                        number
+                                                w = ------------------,
+                                                        total
+
+                            sum 
+        где total = sum + -------- * (n - count) - общее количество вхождений для всех слов ПОЗ;
+                           count 
+
+            number = sum / count - количество вхождений в документ слова ПОЗ, отстутствующего в списке дескрипторов(одинаков для всех слов);
+            sum - сумма количества вхождений в документ слов ПОЗ, присутствующих в списке дескрипторов;
+            count - количество слов ПОЗ, присутствующих в списке дескрипторов;
+            n - количество слов в ПОЗ;
+
+        А вес остальных слов ПОЗ по такой же формуле, но number будет равен числу вхождений этого слова в документ, которое было подсчитано на этапе создания ПОД.
+    */
     static calcWeights(poz, descriptors, n) {
         let sum = 0;
         let count = 0;
-        let num = 0;
+        let number = 0;
+        let weights = [];
+
         for (let i = 0; i < poz.length; i++) {
             let index = descriptors.indexOf(poz[i]);
             if (index !== -1) {
@@ -23,23 +43,23 @@ class SearchEngine {
                 sum += n[index];
             }
         }
-        let arr = [];
 
         if (count === poz.length) {
             for (let i = 0; i < poz.length; i++)
-                arr.push(n[descriptors.indexOf(poz[i])] / sum);
+                weights.push(n[descriptors.indexOf(poz[i])] / sum);
         } else if (count > 0 && count < poz.length) {
-            num = Math.floor(sum / count);
-            sum += num;
+            number = sum / count;
+            sum += number * (poz.length - count);
             for (let i = 0; i < poz.length; i++) {
                 let index = descriptors.indexOf(poz[i]);
-                arr.push((index !== -1) ? n[index] / sum : num / sum);
+                weights.push((index !== -1) ? n[index] / sum : number / sum);
             }
         } else {
             for (let i = 0; i < poz.length; i++)
-                arr.push(1 / poz.length);
+                weights.push(1 / poz.length);
         }
-        return arr;
+
+        return weights;
     }
 
     static calculateR(poz) {
@@ -52,7 +72,6 @@ class SearchEngine {
                 if (doc.type === 'document') {
                     docsCount++;
                     let weights = this.calcWeights(poz, doc.descriptors, doc.n);
-                    console.log(Base64.decodeBase64(localStorage.key(i)), weights);
                     for (let i = 0; i < poz.length; i++) {
                         let dIndex = doc.descriptors.indexOf(poz[i]);
                         if (dIndex !== -1) {
@@ -61,7 +80,7 @@ class SearchEngine {
                             else if (dIndex < 20) r += 1 / 3 * weights[i];
                         }
                     }
-                    r = Math.floor(r / poz.length * 100);
+                    r = Math.round(r / poz.length * 100);
                     docsR[Base64.decodeBase64(localStorage.key(i))] = r;
                 }
             } finally {
