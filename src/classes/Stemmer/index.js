0213c2d814c67c2e65728128f0817aedc4a697c0
Nepein Andrey <nepein96@gmail.com>
2018-11-29 23:26:25 +0300

    fixes

--- src/classes/Stemmer/index.js
+++ src/classes/Stemmer/index.js
@@ -78,22 +78,17 @@ class StemmerClass {
     }
 
     findEnding(endings, word, has = false) {
-        let ending = '';
+        let ending = word;
         let find = [];
         let pos = 0;
-        let lengths = endings[1];
-        if (endings.length === 3)
-            lengths = endings[2];
+        let lengths = (endings.length === 3) ? endings[2] : endings[1];
 
         for (let i = 0; i < lengths.length; i++) {
             if (word.length >= lengths[i]) {
                 pos = word.length - lengths[i];
                 ending = word.slice(pos);
-
                 find = (endings.length === 3) ? ((word.in([1072, 1103], pos - 1) || (word[pos - 1] === undefined && has)) ? endings[0] : endings[1]) : endings[0];
-
-                if (ending.inString(find))
-                    return this.unDoubleN(word.slice(0, word.length - ending.length));
+                if (ending.inString(find)) return this.unDoubleN(word.slice(0, word.length - ending.length));
             }
         }
         return false;
@@ -111,31 +106,27 @@ class StemmerClass {
         if (rv === '') return false; // если RV-часть - пустая строка, то выходим из функции, т.к. работать дальше с пустой строкой бессмысленно
 
         res = this.findEnding(Endings.reflexive, rv);
-        if (res !== false)
-            reflexiveWasFound = true, rv = this.unDoubleN(res);
+        if (res !== false) reflexiveWasFound = true, rv = this.unDoubleN(res);
 
         res = this.findEnding(Endings.adjective, rv);
         if (res !== false) {
-            p = this.findEnding(Endings.participle, res, !!rvhead[rvhead.length - 1].inString(['а', 'я']));
+            p = this.findEnding(Endings.participle, res, rvhead[rvhead.length - 1].inString(['а', 'я']));
             return (p !== false) ? p : res;
         }
 
-        res = this.findEnding(Endings.verb, rv, !!rvhead[rvhead.length - 1].inString(['а', 'я']));
+        res = this.findEnding(Endings.verb, rv, rvhead[rvhead.length - 1].inString(['а', 'я']));
         if (res !== false) return res;
 
         if (!reflexiveWasFound) {
             res = this.findEnding(Endings.noun, rv);
-            if (res !== false)
-                return res;
+            if (res !== false) return res;
         }
 
         return rv;
     }
 
     secondStep(word) {
-        if (word[word.length - 1] === 'и')
-            return word.slice(0, word.length - 1);
-        return word;
+        return (word[word.length - 1] === 'и') ? word.slice(0, word.length - 1) : word;
     }
 
     thirdStep(word) {
@@ -161,9 +152,7 @@ class StemmerClass {
     }
 
     unDoubleN(word) {
-        if (word[word.length - 1] === 'н' && word[word.length - 2] === 'н')
-            return word.slice(0, word.length - 1);
-        return word;
+        return (word[word.length - 1] === 'н' && word[word.length - 2] === 'н') ? word.slice(0, word.length - 1) : word;
     }
 }
 
