9c8c62ad3fd25e69ee68b4d100d0d087c77b6db9
Nepein Andrey <nepein96@gmail.com>
2018-12-05 14:13:01 +0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,61 @@ var path = require('path');
 var ExtractCssPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  mode: 'development',
-  output: {
-    filename: 'main.bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
+    entry: './src/main.js',
+    mode: 'development',
+    output: {
+        filename: 'main.bundle.js',
+        path: path.resolve(__dirname, 'build')
     },
-      {
-        test: /\.less$/,
-        use: [{
-          loader: ExtractCssPlugin.loader
-        },
-          {
-            loader: "css-loader",
-            options: {
-              minimize: true
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                        loader: ExtractCssPlugin.loader
+                    },
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: "postcss",
+                            plugins: () => [
+                                require('autoprefixer')(),
+                                require('cssnano')()
+                            ]
+                        }
+                    },
+                    "less-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|gif)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: 'img',
+                        publicPath: 'img'
+                    }
+                }]
             }
-          },
-          "less-loader"
         ]
-      },
-      //   {
-      //     test: /\.css$/,
-      //     exclude: /node_modules/,
-      //     use: [{
-      //       loader: ExtractCssPlugin.loader
-      //     },
-      //       "css-loader"
-      //     ]
-      //   },
-      {
-        test: /\.(png|svg|gif)$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: "file-loader",
-          options: {
-            outputPath: 'img',
-            publicPath: 'img'
-          }
-        }]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    compress: true,
-    port: 9000
-  },
-  plugins: [
-    new ExtractCssPlugin({
-      filename: "style.css"
-    })
-  ],
-  target: 'web'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new ExtractCssPlugin({
+            filename: "style.css"
+        })
+    ],
+    target: 'web'
 }
